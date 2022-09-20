cebdbeeebfeba071cf66f715ad07bbf46a8c8320
bakiev75 <bakiev75@mail.ru>
2022-09-20 23:01:30 +0500

    add #2 _ 20/09/22

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 # Импортируем класс, который говорит нам о том,
 # что в этом представлении мы будем выводить список объектов из БД
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -9,12 +9,21 @@ class NewsList(ListView):
     # Указываем модель, объекты которой мы будем выводить
     model = Post
     # Поле, которое будет использоваться для сортировки объектов
-    ordering = 'article_or_new'
+    ordering = 'title'
     # Указываем имя шаблона, в котором будут все инструкции о том,
     # как именно пользователю должны быть показаны наши объекты
     template_name = 'news.html'
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news'
+
+class NewDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельной новости
+    model = Post
+    # Используем другой шаблон — new.html
+    template_name = 'new.html'
+    # Название объекта, в котором будет выбранная пользователем новость
+    context_object_name = 'new'
+
 
 # Create your views here.
