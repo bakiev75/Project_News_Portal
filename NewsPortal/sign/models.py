bf5625e769be93db60a7fbea511ddf7fd79933cd
Bakiev75_New <bakiev75@mail.ru>
2022-12-06 02:11:29 +0500

    add D8.5_users_groups

--- NewsPortal/sign/models.py
+++ NewsPortal/sign/models.py
@@ -2,6 +2,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
+
 class BaseRegisterForm(UserCreationForm):
     email = forms.EmailField(label = "Email")
     first_name = forms.CharField(label = "Имя")
@@ -15,3 +18,12 @@ class BaseRegisterForm(UserCreationForm):
                   "email",
                   "password1",
                   "password2", )
+
+
+class BasicSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        basic_group = Group.objects.get(name='common')
+        basic_group.user_set.add(user)
+        return user
